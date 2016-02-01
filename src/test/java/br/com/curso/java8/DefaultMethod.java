52b0cf7ca0fc5441386c8eabcfe56c0bf6d9bcb3
Thiago Leme <leme.thiagoh@gmail.com>
2016-02-01 17:42:00 -0200

    Aula 03 - Method References

--- src/test/java/br/com/curso/java8/DefaultMethod.java
+++ src/test/java/br/com/curso/java8/DefaultMethod.java
@@ -11,27 +11,38 @@ public class DefaultMethod {
 	public void imprimeListTest() {
 		List<String> palavras = criaLista();
 
-		imprimeLista(palavras);
+		 imprimeLista(palavras);
 	}
 
 	@Test
 	public void imprimeOrdenadoTest() {
 		List<String> palavras = criaLista();
 
-		palavras.sort((palavra1, palavra2) -> Integer.compare(palavra1.length(), palavra2.length()));
+		// palavras.sort((palavra1, palavra2) ->
+		// Integer.compare(palavra1.length(), palavra2.length()));
+
+		// palavras.sort(Comparator.comparing(s -> s.length()));
+
+		// palavras.sort(Comparator.comparing(String::toString));
+
+		palavras.sort(String.CASE_INSENSITIVE_ORDER);
 
 		imprimeLista(palavras);
 	}
 
 	private void imprimeLista(List<String> palavras) {
-		palavras.forEach(palavra -> System.out.println(palavra));
+		// palavras.forEach(palavra -> System.out.println(palavra));
+
+		palavras.forEach(System.out::println);
 	}
 
 	private List<String> criaLista() {
 		List<String> palavras = new ArrayList<>();
+
 		palavras.add("alura online");
 		palavras.add("casa do código");
 		palavras.add("caelum");
+
 		return palavras;
 	}
 }
