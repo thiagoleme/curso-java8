80e6f17e3a52ae77248c3ea43d1c5a62668ec0f8
Thiago Henrique Leme <leme.thiagoh@gmail.com>
2016-02-04 23:21:20 -0200

    Aula 05 Streams e Collectors e APIs

--- src/test/java/br/com/curso/java8/CursoTest.java
+++ src/test/java/br/com/curso/java8/CursoTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.junit.Test;
@@ -12,33 +14,12 @@ import org.junit.Test;
 public class CursoTest {
 
 	@Test
-	public void OrdenarPorQtde() {
+	public void calculaMediaDeAlunosPorCurso() throws Exception {
 		List<Curso> cursos = criaCursos();
 
-		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+		OptionalDouble average = cursos.stream().mapToInt(c -> c.getAlunos()).average();
 
-		assertEquals(45, cursos.get(0).getAlunos());
-		assertEquals(55, cursos.get(1).getAlunos());
-		assertEquals(113, cursos.get(2).getAlunos());
-		assertEquals(150, cursos.get(3).getAlunos());
-	}
-
-	@Test
-	public void filtraCursosComMaisDe50Alunos() throws Exception {
-		List<Curso> cursos = criaCursos();
-
-		Stream<Curso> cursoFiltrado = cursos.stream().filter(c -> c.getAlunos() >= 50);
-
-		assertEquals(3, cursoFiltrado.count());
-	}
-
-	@Test
-	public void transformaStreamDeCursoParaStreamDeString() throws Exception {
-		List<Curso> cursos = criaCursos();
-
-		Stream<String> cursoFiltrado = cursos.stream().filter(c -> c.getAlunos() > 50).map(Curso::getNome);
-
-		cursoFiltrado.forEach(System.out::println);
+		System.out.println(average);
 	}
 
 	private List<Curso> criaCursos() {
@@ -50,6 +31,46 @@ public class CursoTest {
 		cursos.add(new Curso("C", 55));
 
 		return cursos;
+	}
+
+	@Test
+	public void filtraCursosComMaisDe50Alunos() throws Exception {
+		List<Curso> cursos = criaCursos();
+
+		Stream<Curso> cursoFiltrado = cursos.stream().filter(c -> c.getAlunos() > 50);
+
+		assertEquals(3, cursoFiltrado.count());
+	}
+
+	@Test
+	public void ordenarPorQtde() {
+		List<Curso> cursos = criaCursos();
+
+		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+
+		assertEquals(45, cursos.get(0).getAlunos());
+		assertEquals(55, cursos.get(1).getAlunos());
+		assertEquals(113, cursos.get(2).getAlunos());
+		assertEquals(150, cursos.get(3).getAlunos());
+	}
+
+	@Test
+	public void StreamParaList() throws Exception {
+		List<Curso> cursos = criaCursos();
+
+		List<Curso> collect = cursos.stream().filter(c -> c.getAlunos() > 50).collect(Collectors.toList());
+
+		System.out.println(collect);
+
+	}
+
+	@Test
+	public void transformaStreamDeCursoParaStreamDeString() throws Exception {
+		List<Curso> cursos = criaCursos();
+
+		Stream<String> cursoFiltrado = cursos.stream().filter(c -> c.getAlunos() > 50).map(Curso::getNome);
+
+		cursoFiltrado.forEach(System.out::println);
 	}
 
 }
